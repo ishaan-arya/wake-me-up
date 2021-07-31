d498de4d0fe2c9bb43d20b41239b9ba609fac181
Ishaan <ishaanarya2004@gmail.com>
2021-07-31 17:45:45 +0530

    [header && QRCodeScreen] => added fonts, made header, QR Code Screen

--- lib/screens/qrCodeScreen/qr_code.screens.dart
+++ lib/screens/qrCodeScreen/qr_code.screens.dart
@@ -1,3 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:wake_me_up/utils/constants/constants.utils.dart';
 import 'package:wake_me_up/widgets/header/header.widget.dart';
+
+class QRCodeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: kPrimaryColor,
+          body: Column(
+            children: [
+              Header(title: 'QR Code Screen'),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: kSecondaryColor,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
