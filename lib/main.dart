ab1f45db2da46f87844e2d7382a3d7530c84f228
Ishaan <ishaanarya2004@gmail.com>
2021-07-31 17:01:00 +0530

    [constants && header] => built the header widget and constants file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:wake_me_up/widgets/header/header.widget.dart';
+import 'package:wake_me_up/utils/constants/constants.utils.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: kPrimaryColor,
+          body: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
 }
---
+++ lib/utils/constants/constants.utils.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF2E2853);
+
+const kSecondaryColor = Color(0xFF62DCFA);
+
+const kContrastColor = Colors.white;
+
+const kContrastColor2 = Color(0xFFC4C4C4);
