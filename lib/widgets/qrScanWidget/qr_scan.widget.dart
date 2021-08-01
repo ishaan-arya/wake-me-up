1873df866b7c1c0cbc7cc694e53514c15b2a45d9
Hasnaink17 <hasnain.koser@gmail.com>
2021-08-01 17:23:27 +0530

    [pubsec.yaml&&qr_scan.widget.dart&&database_management.model.dart] => Fixed dependencies and applied null-safety

--- lib/widgets/qrScanWidget/qr_scan.widget.dart
+++ lib/widgets/qrScanWidget/qr_scan.widget.dart
@@ -1,57 +1,56 @@
-// import 'package:flutter/material.dart';
-// import 'package:qr_code_scanner/qr_code_scanner.dart';
-//
-// class QRScanMode extends StatefulWidget {
-//   final Function setResult;
-//   const QRScanMode({Key key, this.setResult});
-//
-//   @override
-//   _QRScanModeState createState() => _QRScanModeState();
-// }
-//
-// class _QRScanModeState extends State<QRScanMode> {
-//   final qrKey = GlobalKey(debugLabel: 'QR');
-//   QRViewController controller;
-//   Barcode result2;
-//
-//   Widget buildQRView(BuildContext context) {
-//     return QRView(
-//       key: qrKey,
-//       onQRViewCreated: onQRViewCreated,
-//       overlay: QrScannerOverlayShape(
-//         borderColor: Theme.of(context).accentColor,
-//         borderLength: 20,
-//         borderRadius: 10,
-//         borderWidth: 10,
-//       ),
-//     );
-//   }
-//
-//   void onQRViewCreated(QRViewController controller) {
-//     setState(() {
-//       this.controller = controller;
-//     });
-//
-//     controller.scannedDataStream.listen((barcode) {
-//       widget.setResult(barcode);
-//     });
-//   }
-//
-//   @override
-//   void dispose() {
-//     controller?.dispose();
-//     super.dispose();
-//   }
-//
-//   @override
-//   void reassemble() async {
-//     super.reassemble();
-//     await controller?.pauseCamera();
-//     controller?.resumeCamera();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return buildQRView(context);
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class QRScanMode extends StatefulWidget {
+  final Function setResult;
+  const QRScanMode({Key? key, required this.setResult});
+
+  @override
+  _QRScanModeState createState() => _QRScanModeState();
+}
+
+class _QRScanModeState extends State<QRScanMode> {
+  final qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+
+  Widget buildQRView(BuildContext context) {
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+        borderColor: Theme.of(context).accentColor,
+        borderLength: 20,
+        borderRadius: 10,
+        borderWidth: 10,
+      ),
+    );
+  }
+
+  void onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+
+    controller.scannedDataStream.listen((barcode) {
+      widget.setResult(barcode);
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  void reassemble() async {
+    super.reassemble();
+    await controller!.pauseCamera();
+    controller!.resumeCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return buildQRView(context);
+  }
+}
